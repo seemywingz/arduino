edf1dfda1f8ea9927440e097ccea4fb0c66f0e38
Kevin Jayne <kevin.jayne@icloud.com>
2023-10-07 11:36:45 -0400

    gitignore

--- blink/blink.ino
+++ blink/blink.ino
@@ -2,16 +2,14 @@
 #include <Pin.h>
 
 Pin *p = new Pin(LED_BUILTIN);
-int mills = 100;
+int mills = 1000;
 
-void setup()
-{
-};
+void setup(){};
 
 void loop()
 {
     p->on();
-    delay(mills);           // wait for a second
-    p->off(); 
+    delay(mills);
+    p->off();
     delay(mills);
 };
