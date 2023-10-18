1d97f96d4e6c8c5f1aa33262da06cca93c977811
Kevin Jayne <kevin.jayne@icloud.com>
2023-10-17 22:04:05 -0400

    not verbose

--- led_matrix_audio_spectrum/led_matrix_audio_spectrum.ino
+++ led_matrix_audio_spectrum/led_matrix_audio_spectrum.ino
@@ -160,7 +160,7 @@ void drawBars(int *spectralData) {
 void initButtonHandlers() {
   attachInterrupt(
       digitalPinToInterrupt(btn1.pin()), []() { btn1.isr(); }, CHANGE);
-  btn1.setVerbose(true);
+  // btn1.setVerbose(true);
   btn1.setSingleClickCallback([]() {
     if (btn2Pin.readD() == LOW) {
       Serial.println("Changing color pallet");
